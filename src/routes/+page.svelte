8592636d0fa2d6374759727de14a2930c7bab3d2
Gareth Davies <gaz.andrew@gmail.com>
2025-07-01 17:39:17 +0100

    Refactor +page.svelte to improve file upload handling and display uploaded Excel data

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,16 +2,23 @@
     import { readExcel } from '$lib/utils/readExcel';
     import Hearder from '$lib/components/Hearder.svelte';
     import FileUpload from '$lib/components/FileUpload.svelte';
+
     /**
-     * 
+     * @type {Array<any>}
+     */
+    let data= $state([]);
+
+    
+
+    /**
      * @param {Event} event 
      */
-
     async function handleFileUpload(event) {
       if (!event.target || !(event.target instanceof HTMLInputElement)) return;
       const file = event.target.files?.[0];
       if (!file) return;
-      const data = await readExcel(file);
+      const excelData = await readExcel(file);
+      data=excelData;
       console.log(data);
     }
   </script>
@@ -20,3 +27,4 @@
 
 <FileUpload handleFileUpload={handleFileUpload} />
 
+<pre>{JSON.stringify(data, null, 2)}</pre>
